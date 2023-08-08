19700291578602ac566961cf1e9e43ace46bc8a3
Andrew <andrewhoonchang97@gmail.com>
2023-08-08 12:06:10 -0700

    Adding exercise 9. I definitely overthought it but I also needed to look up a lot of stuff for this one. Maybe I'll use the code with replace (/[^A-Za-z0-9]/g again soon, I understand the reasoning but not why it's used

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+        const cleanedWord = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+        const reversedWord = cleanedWord.split('').reverse().join('');
+        return cleanedWord === reversedWord;
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
