f2a8180e99ce6f228baff89c1f575d72d854248a
Andrew <andrewhoonchang97@gmail.com>
2023-08-07 23:48:26 -0700

    Adding exercise 7, I'm really enjoying these assingments now that I'm understanding what to use, where to use, and why. I'll revisit exercise 4 at the end and see what I can do differently

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celsiusConverted = (fahrenheit - 32) * (5/9)
+  if (celsiusConverted > Math.floor(celsiusConverted)){
+    return Math.round(10*celsiusConverted)/10;
+  } else {
+    return celsiusConverted;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitConverted = (celsius*(9/5)+32);
+  if (fahrenheitConverted > Math.floor(fahrenheitConverted)){
+    return Math.round(10*fahrenheitConverted)/10;
+  } else {
+    return fahrenheitConverted;
+  }
 };
 
 // Do not edit below this line
