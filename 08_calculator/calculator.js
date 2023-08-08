1725ab6a209444e383f1a7397115e8665d623e22
Andrew <andrewhoonchang97@gmail.com>
2023-08-08 00:42:36 -0700

    Added exercise 8. I'm feeling really good about these exercises, hopefully I don't hit a roadblock soon

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x,y) {
+	let answerAdd = x + y;
+  return answerAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	let answerSubt = x - y;
+  return answerSubt;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  let answerSum = 0
+  if (sumArray.length > 0){
+  for (let i=0; i<sumArray.length;i++){
+    answerSum += sumArray[i];
+  }
+  return answerSum;
+} else {
+  return 0;
+}
 };
 
-const multiply = function() {
-
+const multiply = function(mulArray) {
+  let multStart = 1;
+  for (i = 0; i < mulArray.length; i++)
+    multStart = multStart * mulArray[i];
+  return multStart
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let functionPower = Math.pow(a,b)
+  return functionPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNum) {
+	if (factorialNum < 0){
+    return "ERROR"
+  } else if (factorialNum === 0){
+    return 1;
+  } else if (factorialNum > 0){
+    for (i = factorialNum - 1; i >= 1; i--){
+    factorialNum = factorialNum * i
+    }
+    return factorialNum;
+  }
 };
 
 // Do not edit below this line
