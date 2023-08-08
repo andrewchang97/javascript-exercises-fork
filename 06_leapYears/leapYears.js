cb992cf07b449ce624ef1c7c23efd23e5d75915c
Andrew <andrewhoonchang97@gmail.com>
2023-08-07 18:42:11 -0700

    Leap year assignment was something I understood well and was able to get going fairly easily. Code is clean and I feel good about it

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (years % 10 === 0 && years % 400 != 0){
+            return false;
+        } else if (years % 4 === 0){
+            return true;
+        } else {
+            return false;
+        }
 };
 
 // Do not edit below this line
