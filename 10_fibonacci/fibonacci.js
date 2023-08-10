80bf637f038134a5fde49f49070c54c03df1442b
Andrew <andrewhoonchang97@gmail.com>
2023-08-10 14:58:51 -0700

    Adding exercise 10. I managed to do it on my own with no help, and the finished solution looks extremely similar to my own. I'm glad to be back to feeling like I know what I'm doing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    let initial = 0;
+    let add = 1;
+    let sum;
+    let number = parseInt(place)
+for (i = 0; i < (number - 1); i++) {
+    sum = initial + add;
+    initial = add;
+    add = sum;
+} if (sum > 0 && number > 0){
+    return sum;
+} else if (sum = 1 && number > 0) {
+    return 1
+} else {
+    return "OOPS"
+}
 };
 
 // Do not edit below this line
